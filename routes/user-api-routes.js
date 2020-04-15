388034d38187715576e57be920a5385df31dd958
drospond <david.rospond@gmail.com>
2020-04-15 18:45:52 -0400

    Commented started code for steam API calls

---
+++ routes/user-api-routes.js
@@ -0,0 +1,17 @@
+//get user info
+//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=F83481CE586D90CD0924F6576AA13DEC&steamids=76561198071626037
+
+//get user id by steam name
+//"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=58B845E0858DDB3A73B47D71B787198A&vanityurl=sammysticks"
+
+
+// var db = require("../models");
+// const apiKey = process.env.API_KEY;
+
+//sample axios call
+// .get("https://icanhazdadjoke.com/", config)
+// .then(function(res) {
+//   const { joke } = res.data;
+
+//   return appendFileAsync("jokes.txt", joke + "\n");
+// })
