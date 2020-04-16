b48907362665f0c8cb2c3529f644c96c01e1a381
Alan Ashworth <awashworth927@gmail.com>
2020-04-16 14:30:35 -0400

    Deveop api routes to take in username and retrieve user id. Devlop api routes to post new user to database.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,8 @@
 module.exports = function(app) {
-
   app.get("/", function (req, res) {
     res.render("index");
   });
-
 };
+
+// What do we use this file for? Is it only to render the index.handlerbars? think so
+// Then let's consider this file done for now.
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 require("./routes/html-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
 
 // Import routes and give the server access to them.
 // const catRoutes = require("./controllers/catsController.js");
