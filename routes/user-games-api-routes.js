8229cd7ffd3fd200ca85bc00a8513b0508a0166c
jessie-computes <60405600+jessie-computes@users.noreply.github.com>
2020-04-17 13:30:47 -0400

    Updated user-games-api-routes

--- routes/user-games-api-routes.js
+++ routes/user-games-api-routes.js
@@ -5,14 +5,14 @@
 //https://store.steampowered.com/api/appdetails?appids=7670
 //"categories" {id: 1, description: "Multi-player"}
 
-var db = require("../models");
-module.exports = function (app) {
-  const axios = require("axios");
-  const apiKey = process.env.API_KEY || "58B845E0858DDB3A73B47D71B787198A";
+// var db = require("../models");
+// module.exports = function (app) {
+//   const axios = require("axios");
+//   const apiKey = process.env.API_KEY || "58B845E0858DDB3A73B47D71B787198A";
 
-  function getGamesList(){
-    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=76561198071626037&format=json&include_appinfo=true`;
+//   function getGamesList(apiKey,steamID){
+//     const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_appinfo=true`;
 
-    axios.get(url).then();
-  }
-}
+//     axios.get(url).then();
+//   }
+// }
