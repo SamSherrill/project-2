0b581d42a6e9a30d0f535687df5a686fc112acd3
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-04-18 15:46:30 -0400

    Jonathan rewrote some code & we cut the scope of our project down to not include multiplayer as a table in our games model

--- routes/user-games-api-routes.js
+++ routes/user-games-api-routes.js
@@ -5,83 +5,9 @@
 //https://store.steampowered.com/api/appdetails?appids=7670
 //"categories" {id: 1, description: "Multi-player"}
 
-var db = require("../models");
-module.exports = function (app) {
-    const axios = require("axios");
-    const apiKey = process.env.API_KEY;
-
-    const steamID = "76561198035672130";
-    // that steamID is hard coded for sammysticks' Steam ID #. ID numbers are required to hit the API.
-
-    // function getGamesList(apiKey, steamID) {
-    //const ownedGamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_appinfo=true`;
-
-    //axios.get(ownedGamesUrl).then((res) => {
-    //   for (var i = 0; i < res.data.response.games.length; i++) {
-    //     //check if in database
-    //     const gameId = res.data.response.games[i].appid;
-    //     db.Game.findOne({
-    //       where: {
-    //         name: res.data.response.games[i].name,
-    //       },
-    //     }).then((gameRes) => {
-    //       if (!gameRes) {
-    //         //axios call game info
-    //         // console.log("Game ID: ", res.data.response.games[i]);
-    //         const gameInfoUrl = `https://store.steampowered.com/api/appdetails?appids=${gameId}`;
-    //         axios.get(gameInfoUrl).then(gameInfoRes => {
-    //           console.log(gameInfoRes);
-    //           //check if multiplayer
-    //           //appid.data.categories
-
-    //           // for(var i = 0; i < gameInfoRes.data.length; i++){
-    //           //   var appId = gameInforRes.data[i];
-    //           //   console.log(appId);
-    //           // }
-    //           // if(gameInfoRes.data);
-    //           const banana = `gameInfoRes.data.${gameId}.data.categories[0]`;
-    //           console.log(banana);
-    //           //https://store.steampowered.com/api/appdetails?appids=440
-    //         });
-    //       } else {
-    //         console.log("Game exists");
-    //       }
-    //     });
-    //   }
-    // });
-    //}
-
-    app.get("/api/games", ((req, res) => {
-        // getGamesList(apiKey, steamID);
-        const ownedGamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_appinfo=true`;
-
-        axios.get(ownedGamesUrl).then(async (response) => {
-            console.log(response.data.response.games);
-            // res.json(response.data.response.games);
-            for (let i = 0; i < response.data.response.games.length; i++) {
-
-                const newGame = await db.Game.create({
-                    name: response.data.response.games[i].name,
-                    appId: response.data.response.games[i].appid
-                });
-                await console.log(newGame);
-                await console.log("created a game. Mayabe?");
-            }
-            await console.log("=============");
-            await console.log("this should wait until the end");
-            await res.json({
-                success: true
-            });
-        }).catch(err => {
-            console.log(err);
-        });
-    }));
-
-};
 
 
-
-//pseudo code:
+//pseudo code from David Saturday morning:
 //Make API call to get games owned list: http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=F83481CE586D90CD0924F6576AA13DEC&steamid=76561198071626037&format=json&include_appinfo=true
 //loop through games
 ////Check if game is in database
@@ -90,6 +16,94 @@ module.exports = function (app) {
 ////save (post) info to database
 // - need to get user ID #s dynamically
 
+// PSEUDO CODE from Sam Saturday afternoon:
+// Need to pull in user ID #s dynamically & use them with the const steamID below
+// Then we need to post to our DB every owned game of that user
+// and then make sure the many to many relationship is correct between the user & their games
+
+var db = require("../models");
+module.exports = function (app) {
+  const axios = require("axios");
+  const apiKey = process.env.API_KEY;
+
+  const steamID = "76561198035672130";
+  // that steamID is hard coded for sammysticks' Steam ID #. ID numbers are required to hit the API.
+
+  app.get("/api/games", ((req, res) => {
+    // getGamesList(apiKey, steamID);
+    const ownedGamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_appinfo=true`;
+
+    axios.get(ownedGamesUrl).then(async (response) => {
+      let singleGame = {};
+      for (let i = 0; i < response.data.response.games.length; i++) {
+        const gameFromDatabase = await db.Game.findOne({
+          where: {
+            appId: response.data.response.games[i].appid
+          }
+        });
+        if (gameFromDatabase === null) {
+          // TODO: Make an API call,
+          // wait for the call to complete,
+          //  then update the db.Game.create to include that additional data
+          singleGame = await db.Game.create({
+            steamid: this.steamID,
+            name: response.data.response.games[i].name,
+            appId: response.data.response.games[i].appid
+          });
+        } else {
+          singleGame = gameFromDatabase;
+        }
+        await console.log(singleGame);
+
+      }
+      await res.json({
+        success: true
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  }));
+
+  // old code that Jonathan's code supercedes:
+  // function getGamesList(apiKey, steamID) {
+  //const ownedGamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_appinfo=true`;
+
+  //axios.get(ownedGamesUrl).then((res) => {
+  //   for (var i = 0; i < res.data.response.games.length; i++) {
+  //     //check if in database
+  //     const gameId = res.data.response.games[i].appid;
+  //     db.Game.findOne({
+  //       where: {
+  //         name: res.data.response.games[i].name,
+  //       },
+  //     }).then((gameRes) => {
+  //       if (!gameRes) {
+  //         //axios call game info
+  //         // console.log("Game ID: ", res.data.response.games[i]);
+  //         const gameInfoUrl = `https://store.steampowered.com/api/appdetails?appids=${gameId}`;
+  //         axios.get(gameInfoUrl).then(gameInfoRes => {
+  //           console.log(gameInfoRes);
+  //           //check if multiplayer
+  //           //appid.data.categories
+
+  //           // for(var i = 0; i < gameInfoRes.data.length; i++){
+  //           //   var appId = gameInforRes.data[i];
+  //           //   console.log(appId);
+  //           // }
+  //           // if(gameInfoRes.data);
+  //           const banana = `gameInfoRes.data.${gameId}.data.categories[0]`;
+  //           console.log(banana);
+  //           //https://store.steampowered.com/api/appdetails?appids=440
+  //         });
+  //       } else {
+  //         console.log("Game exists");
+  //       }
+  //     });
+  //   }
+  // });
+  //}
+
+};
 
 
 // game.js model:
--- server.js
+++ server.js
@@ -10,13 +10,17 @@ const db = require("./models");
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 require("./routes/handlebars-routes.js")(app);
