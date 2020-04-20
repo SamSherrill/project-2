4eec80fd833b55b1c592d5e678f3e91541564e21
drospond <david.rospond@gmail.com>
2020-04-19 22:08:37 -0400

    Page loads after games are added to DB

--- public/js/index.js
+++ public/js/index.js
@@ -23,105 +23,28 @@ $(document).ready(function () {
           userOne: userOne,
           userTwo: userTwo,
         }).done((res) => {
-          console.log(res);
-          window.location.href = "/SteamUsers/" + userOne + "/" + userTwo;
+          $.post("/api/games", {
+            user: userOne,
+          }).done(()=>{
+            $.post("/api/games", {
+              user: userTwo,
+            }).done(()=> window.location.href = "/SteamUsers/" + userOne + "/" + userTwo);
+          });
         })
       );
-      $.post("/api/games", {
-        user: userOne,
-      });
-      $.post("/api/games", {
-        user: userTwo,
-      });
     } else if (userOne) {
       $.post("/api/steamUsers", {
         user: userOne,
       }).then(
         $.get("/SteamUser/" + userOne, {
           userOne: userOne,
-        }).done((response) => {
-          console.log(response);
-          window.location.href = "/SteamUser/" + userOne;
+        }).done(() => {
+          $.post("/api/games", {
+            user: userOne,
+          }).done(()=>window.location.href = "/SteamUser/" + userOne);
         })
       );
-      $.post("/api/games", {
-        user: userOne,
-      });
     }
-    //The following block of user code is done in the user-api-routes.js
-    // We need to save the usernames
-    // Some other file is going to have to 1st check our DB for that user
-    //
 
-    // Step 2 or beyond:
-    // Then this other file is oging to need to go to the steam API
-    // and get the user's ID #
-    // then we'll need to use the user ID # to get the user's game's list
-    // Steam API call this GetOwnedGames ---
-
-    // user-api-routes.js will res.render("SteamUser")
   });
 });
-
-// cat.js example, which was the index.js for that activity:
-
-// // Make sure we wait to attach our handlers until the DOM is fully loaded.
-// $(function() {
-//     $(".change-sleep").on("click", function(event) {
-//       var id = $(this).data("id");
-//       var newSleep = $(this).data("newsleep");
-
-//       var newSleepState = {
-//         sleepy: newSleep
-//       };
-
-//       // Send the PUT request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "PUT",
-//         data: newSleepState
-//       }).then(
-//         function() {
-//           console.log("changed sleep to", newSleep);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-
-//     $(".create-form").on("submit", function(event) {
-//       // Make sure to preventDefault on a submit event.
-//       event.preventDefault();
-
-//       var newCat = {
-//         name: $("#ca").val().trim(),
-//         sleepy: $("[name=sleepy]:checked").val().trim()
-//       };
-
-//       // Send the POST request.
-//       $.ajax("/api/cats", {
-//         type: "POST",
-//         data: newCat
-//       }).then(
-//         function() {
-//           console.log("created new cat");
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-
-//     $(".delete-cat").on("click", function(event) {
-//       var id = $(this).data("id");
-
-//       // Send the DELETE request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "DELETE"
-//       }).then(
-//         function() {
-//           console.log("deleted cat", id);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-//   });
